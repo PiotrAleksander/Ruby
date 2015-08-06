d749e292be9ca286251710398788aed3b199c743
PiotrAleksander <piotrhugonow@gmail.com>
2015-08-06 13:58:00 +0200

    fibonacci iteracyjnie

--- fibonacci.rb
+++ fibonacci.rb
@@ -4,13 +4,26 @@ def fib(n, cache = {})
   end
   cache[n] ||= fib(n-1, cache) + fib(n-2, cache)
 end
+
+def fibs(num)
+    i = 0
+    nast = 1
+    j = 0
+    while (num -= 1) > 0 do
+        j = i + nast
+        i = nast
+        nast = j
+    end
+    return nast
+end
 puts "Która liczba w ciągu Fibonacciego?"
 licz = gets.chop
 while licz.downcase != "koniec"
 puts fib licz.to_i
+puts fibs licz.to_i
 licz = gets.chop
 end
-puts "Na koniec jeszcze 14 i 24 element ciągu:"
+puts "Na koniec nasteszcze 14 i 24 element ciągu:"
 puts fib 14
 # => 377
 puts fib 24
